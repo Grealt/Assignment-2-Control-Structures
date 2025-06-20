2e130080150d3aed0c792b0397471ac7944973d5
Your Name <you@example.com>
2025-06-20 09:06:04 +0530

    Added even/odd checker and sum from 1 to 50 (Assignment 2)

---
+++ .venv/even_odd_checker.py
@@ -0,0 +1,8 @@
+# Task 1: Check if a Number is Even or Odd
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print(f"\n{number} is an even number.")
+else:
+    print(f"\n{number} is an odd number.")
---
+++ .venv/sum_1_to_50.py
@@ -0,0 +1,7 @@
+# Task 2: Sum of Integers from 1 to 50 Using a Loop
+
+total = 0
+for num in range(1, 51):
+    total += num
+
+print(f"\nThe sum of numbers from 1 to 50 is: {total}")
