d6fa351ed9e4c7bfe9afff4874b09371967767c0
Your Name <you@example.com>
2025-07-01 16:40:45 +0530

    Added even/odd checker and sum from 1 to 50 (Assignment 1)

---
+++ .venv/greeting.py
@@ -0,0 +1,9 @@
+# Task 2: Create a Personalized Greeting
+
+# Taking user input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Creating full name and greeting message
+full_name = first_name + " " + last_name
+print(f"\nHello, {full_name}! Welcome to the Python program.")
